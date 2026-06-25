7eb907b2698d1527e7d0e90ea80fb0b80aae22f7
Fátima Carla <amitafccs@gmail.com>
2026-06-25 18:38:54 -0300

    versao nova

--- script.js
+++ script.js
@@ -76,5 +76,60 @@ function initializeNotes() {
         }
     });
 }
+
+//document.getElementsByTagName('p')[0].textContent = getBackupNote();
 //teste
 document.addEventListener('DOMContentLoaded', initializeNotes);
+
+let xis = 0
+let egoe = 0
+const botao = document.getElementById("butao")
+const reinicia = document.getElementById("res")
+reinicia.style.display = "none"
+botao.style.width = "300px";
+botao.style.height = "75px";
+botao.style.fontSize = "35px";
+botao.style.borderRadius = "50%";
+botao.style.backgroundColor = "var(--primary)";
+
+reinicia.addEventListener("click", function (){
+  xis = 0
+  document.getElementById("sla").textContent = "quantidade de aura: " + xis
+  document.getElementById("z").textContent= ""
+  this.style.display = "none"
+})
+
+function mousefora() {
+  becap = egoe
+  egoe = 0
+  document.getElementById("z").textContent= "+" + becap + " de aura"
+  botao.style.backgroundColor = "var(--primary)"
+  botao.style.color = "var(--panel)"
+}
+function aura() {
+  xis += 10
+  egoe += 10
+  document.getElementById("sla").textContent = "quantidade de aura: " + xis
+  document.getElementById("z").textContent= "+" + egoe + " de aura"
+  botao.style.backgroundColor = "var(--accent)";
+  botao.style.color = "var(--panel)"
+  reinicia.style.display = "block"
+}
+function ego() {
+  xis += 100000000
+  document.getElementById("sla").textContent = "quantidade de aura: " + xis
+   document.getElementById("z").textContent= "+100000000 de aura"
+  botao.style.backgroundColor = "var(--muted)"
+  botao.style.color = "var(--panel)"
+  tiratexto()
+}
+function menosaura() {
+  let calmacalabreso = 0
+  calmacalabreso += ((Math.round(Math.random()*10 + 10))*(Math.round(Math.random() * 10) + 10))*10
+  if (calmacalabreso < 0) { calmacalabreso *= -1 } else {}
+  xis -= calmacalabreso
+  document.getElementById("sla").textContent = "quantidade de aura: " + xis
+  document.getElementById("z").textContent= "-" + calmacalabreso + " de aura"
+  botao.style.backgroundColor = "var(--panel)";
+  botao.style.color = "var(--muted)"
+}
